8ab2599e8c895510ae60d5fc1abf5b901b24cdb7
Artyom Sergeev <aosergeev@yandex.ru>
2025-03-20 12:19:24 +0300

    add plug to vim installation

--- install.sh
+++ install.sh
@@ -3,17 +3,14 @@ CUR_DIR=$(pwd)
 
 #kitty
 CONF_KITTY=~/.config/kitty/kitty.conf
-rm -f $CONF_KITTY
 cp $CUR_DIR/kitty/kitty.conf $CONF_KITTY
 
 #bash
 CONF_BASH=~/.bashrc
-rm -f $CONF_BASH
 cp $CUR_DIR/bash/bashrc $CONF_BASH
 
 #tmux
 CONF_TMUX=~/.tmux.conf
-rm -f $CONF_TMUX
 cp $CUR_DIR/tmux/tmux.conf $CONF_TMUX
 
 #kmonad
@@ -22,8 +19,23 @@ DEV_KMONAD=$CUR_DIR/kmonad/config.kbd
 kmonad -d $DEV_KMONAD
 errcode=$? 
 if [ $errcode -eq 0 ]; then 
- rm -f $CONF_KMONAD
  cp $DEV_KMONAD $CONF_KMONAD 
 else
- echo error
+ echo Checking kmonad config exit with non zero code
 fi
+
+#vim
+CONF_VIM=~/.vimrc
+DEV_VIM=$CUR_DIR/vim/init.vim
+AUTOLOAD_PATH_VIM=~/.vim/autoload/
+DEV_AUTOLOAD_PATH_VIM=$CUR_DIR/vim/autoload/*
+#vim -u NONE -c 'try | source $DEV_AUTOLOAD_PATH_VIM | source $DEV_VIM | catch | cq | endtry | q'
+#errcode=$?
+#if [ $errcode -eq 0 ]; then
+mkdir -p $AUTOLOAD_PATH_VIM 
+cp $DEV_AUTOLOAD_PATH_VIM $AUTOLOAD_PATH_VIM
+cp $DEV_VIM $CONF_VIM
+#else 
+#echo Checking vim config exited with non zero code
+#fi 
+
