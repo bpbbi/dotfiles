f4cbc9d5673110aac70a298fab16e48d62df5065
Artyom Sergeev <aosergeev@yandex.ru>
2025-04-15 10:40:33 +0300

    add git convig fix zsh config

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ cp $CUR_DIR/kitty/kitty.conf $CONF_KITTY
 #bash
 CONF_BASH=~/.bashrc
 cp $CUR_DIR/bash/bashrc $CONF_BASH
+
 #zsh
 CONF_ZSH=~/.zshrc
 cp $CUR_DIR/zsh/zshrc $CONF_ZSH
+
 #tmux
 CONF_TMUX=~/.tmux.conf
 cp $CUR_DIR/tmux/tmux.conf $CONF_TMUX
@@ -39,4 +41,9 @@ mkdir -p $VIM_AUTOLOAD_DIR
 cp $DEV_VIM_AUTOLOAD_FILES $VIM_AUTOLOAD_DIR
 cp $DEV_VIM_RC $DEV_VIM_CONF_FILES $VIM_CONF_DIR
 vim -c 'PlugInstall' -c 'qa'
+
+# git
+GIT_CONFIG=~/.gitconfig
+DEV_GITCONFIG_FILE=$CUR_DIR/git/gitconfig
+cp $DEV_GITCONFIG_FILE $GIT_CONFIG
 
