974fb70bc0dba2c90292b5beb2e1aec0cc08e601
Artyom Sergeev <aosergeev@yandex.ru>
2026-03-16 17:27:55 +0300

    clean install

--- install.sh
+++ install.sh
@@ -26,56 +26,24 @@ CONF_KMONAD=~/.config/kmonad/
 DEV_KMONAD=$CUR_DIR/kmonad/config.kbd
 DEV_KMONAD2=$CUR_DIR/kmonad/config2.kbd
 kmonad -d $DEV_KMONAD
-kmonad -d $DEV_KMONAD2
 errcode=$? 
 if [ $errcode -eq 0 ]; then 
   cp $DEV_KMONAD $CONF_KMONAD 
-  cp $DEV_KMONAD2 $CONF_KMONAD 
 else
   echo Checking kmonad config exit with non zero code
 fi
-
-#vim
-# VIM_CONF_DIR=~/.vim/
-# VIM_AUTOLOAD_DIR=$VIM_CONF_DIR/autoload/
-# VIM_COLOURS_DIR=$VIM_CONF_DIR/colors/
-#
-# DEV_VIM_CONF_FILES=$CUR_DIR/vim/*.vim
-# DEV_VIM_RC=$CUR_DIR/vim/vimrc
-# DEV_VIM_AUTOLOAD_FILES=$CUR_DIR/vim/autoload/*
-# DEV_VIM_COLORS_FILES=$CUR_DIR/vim/colors/*.vim
-# mkdir -p $VIM_AUTOLOAD_DIR
-# mkdir -p $VIM_COLOURS_DIR
-
-# cp $DEV_VIM_AUTOLOAD_FILES $VIM_AUTOLOAD_DIR
-# cp $DEV_VIM_RC $DEV_VIM_CONF_FILES $VIM_CONF_DIR
-# cp $DEV_VIM_COLORS_FILES $VIM_COLOURS_DIR;
-# vim -c 'PlugInstall' -c 'qa'
+kmonad -d $DEV_KMONAD2
+errcode=$? 
+if [ $errcode -eq 0 ]; then 
+  cp $DEV_KMONAD2 $CONF_KMONAD 
+else
+  echo Checking kmonad2 config exit with non zero code
+fi
 
 #nvim
 NVIM_CONF_DIR=~/.config/nvim/
-# NVIM_AUTOLOAD_DIR=$NVIM_CONF_DIR/autoload/
-# NVIM_COLOURS_DIR=$NVIM_CONF_DIR/colors/
-
 rm -rf $NVIM_CONF_DIR
 cp -r $CUR_DIR/nvim ~/.config
-
-# DEV_NVIM_CONF_FILES=$CUR_DIR/nvim/*.vim
-# DEV_NVIM_MODS_CONF_FILES=$CUR_DIR/nvim/*.json
-# DEV_NVIM_AUTOLOAD_FILES=$CUR_DIR/nvim/autoload/*
-# DEV_NVIM_COLORS_FILES=$CUR_DIR/nvim/colors/*.vim
-# DEV_NVIM_LUA=$CUR_DIR/nvim/colors/*.lua
-# mkdir -p $NVIM_AUTOLOAD_DIR
-# mkdir -p $NVIM_COLOURS_DIR
-
-
-
-# cp $DEV_NVIM_MODS_CONF_FILES $NVIM_CONF_DIR
-# cp $DEV_NVIM_CONF_FILES $NVIM_CONF_DIR
-# cp $DEV_NVIM_AUTOLOAD_FILES $NVIM_AUTOLOAD_DIR
-# cp $DEV_NVIM_COLORS_FILES $NVIM_COLOURS_DIR;
-# cp $DEV_NVIM_LUA $NVIM_CONF_DIR
-# nvim -c 'PlugInstall' -c 'qa'
 
 # git
 GIT_CONFIG=~/.gitconfig
